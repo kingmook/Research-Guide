0beddac9b52ca07368b8364b5cf64b5e678d8007
kingmook <mbrousseau@ctlet.brocku.ca>
2014-09-03 09:18:21 -0400

    Adding choice of API version. Set $apiVersion to 1 or 2.

--- info.php
+++ info.php
@@ -9,14 +9,14 @@ $host = "";
 $database = "";
 
 //The LTI Credentials for index.php
-//You need to set the LTI Key and LTI Secret
+//You need to set the LTI Key and LTI Secret (no longer than 32)
 $ltiKey = "YourKeyHere";
 $ltiSecret = "YourSecretHere";
 
 $lti_auth = array('key' => $ltiKey, 'secret' => $ltiSecret);
 
 //The secret we're using for the time hashing of ajax requests
-//Should be at least 12 characters long
+//Should be 12 characters long
 $timeKey = 'Imtwelvechar';
 
 //The hosting server url to help validate ajax query destination
@@ -27,9 +27,13 @@ $defaultGuide = 'https://www.adefaultwebsite.com';
 
 //API Integrations for LibGuide v.1
 //If the LibGuide API should be available for use in the Admin panel for manual refresh
-//and the URL to the bulleted list API listing for LibGuides
 $apiAvailable = TRUE;
+
+//URL to the bulleted list API listing for LibGuides
 $apiURL = "http://api.libguides.com/your_api_link_here";
+
+//Use LibGuide api V1 or V2. Set to 1 or 2
+$apiVersion = 2;
 
 $rawAPI = file_get_contents($apiURL);
 
